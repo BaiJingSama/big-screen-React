462371e18484edb1fa2896a21efbadf50aa6008f
Jackeybai <1303802862@qq.com>
2023-01-10 17:34:40 +0800

    完成 chart-14 表格的制作

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -10,11 +10,12 @@ import { Chart5 } from "../components/chart-5";
 import { Chart6 } from "../components/chart-6";
 import { Chart7 } from "../components/chart-7";
 import { Chart8 } from "../components/chart-8";
-import { Chart9 } from "../components/chart.9";
+import { Chart9 } from "../components/chart-9";
 import { Chart10 } from "../components/chart-10";
 import { Chart11 } from "../components/chart-11";
 import { Chart12 } from "../components/chart-12";
 import { Chart13 } from "../components/chart-13";
+import { Chart14 } from "../components/chart-14";
 
 export const Home = () => {
   const year = new Date().getFullYear();
@@ -44,7 +45,7 @@ export const Home = () => {
             </div>
           </div>
         </section>
-        <section className="bordered section5">
+        <section className="section5">
           <div className="bordered row1 案发类型">
             <h2>案发类型统计</h2>
             <div className="charts">
@@ -63,6 +64,7 @@ export const Home = () => {
 
           <div className="bordered row3 作案手段">
             <h2>作案手段分析</h2>
+            <Chart14 />
           </div>
         </section>
       </main>
