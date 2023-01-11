ee3ee471400da88dbcf8c0122004ef5a43b9d1fb
Jackeybai <1303802862@qq.com>
2023-01-11 12:12:56 +0800

    完成数据更新的展示

--- src/components/chart-2.tsx
+++ src/components/chart-2.tsx
@@ -3,12 +3,54 @@ import * as echarts from "echarts";
 import { px } from "../shared/px";
 import { baseEchartOptions } from "../shared/base-echart-options";
 import { createEchartOptions } from "../shared/create-echart-options";
+import { getRandom } from "../shared/getRandom";
 
 export const Chart2 = () => {
   const divRef = useRef(null);
+  const myChart = useRef(null);
+  const data = [
+    { name: "武侯区公安局", 2021: 5, 2022: 3 },
+    { name: "金牛区公安局", 2021: 1, 2022: 4 },
+    { name: "高新区公安局", 2021: 3, 2022: 5 },
+    { name: "成华区公安局", 2021: 1, 2022: 1 },
+    { name: "锦江区公安局", 2021: 4, 2022: 8 },
+    { name: "双流区公安局", 2021: 5, 2022: 4 },
+    { name: "新都区公安局", 2021: 6, 2022: 5 },
+    { name: "郫都区公安局", 2021: 7, 2022: 6 },
+  ];
   useEffect(() => {
-    var myChart = echarts.init(divRef.current);
-    myChart.setOption(
+    setInterval(() => {
+      const newData = [
+        {
+          name: "武侯区公安局",
+        },
+        {
+          name: "金牛区公安局",
+        },
+        {
+          name: "高新区公安局",
+        },
+        {
+          name: "成华区公安局",
+        },
+        {
+          name: "锦江区公安局",
+        },
+        {
+          name: "双流区公安局",
+        },
+        {
+          name: "新都区公安局",
+        },
+        {
+          name: "郫都区公安局",
+        },
+      ];
+      x(newData);
+    }, 3000);
+  }, []);
+  const x = (data) => {
+    myChart.current.setOption(
       createEchartOptions({
         ...baseEchartOptions,
         grid: {
@@ -27,17 +69,7 @@ export const Chart2 = () => {
         yAxis: {
           axisTick: { show: false },
           type: "category",
-          data: [
-            "武侯区公安局",
-            "金牛区公安局",
-            "高新区公安局",
-            "成华区公安局",
-            "锦江区公安局",
-            "双流区公安局",
-            "龙泉驿区公安局",
-            "温江区公安局",
-            "郫都区公安局",
-          ],
+          data: data.map((i) => i.name),
           axisLabel: {
             formatter(val) {
               return val.replace("公安局", "\n公安局");
@@ -48,7 +80,10 @@ export const Chart2 = () => {
           {
             name: "2021年",
             type: "bar",
-            data: [42, 41, 36, 65, 35, 18, 24, 36, 40],
+            data: data.map((i) => {
+              i[2021] = getRandom(3, 10);
+              return i[2021];
+            }),
             itemStyle: {
               normal: {
                 color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [
@@ -67,7 +102,10 @@ export const Chart2 = () => {
           {
             name: "2022年",
             type: "bar",
-            data: [49, 57, 36, 55, 30, 12, 20, 39, 48],
+            data: data.map((i) => {
+              i[2022] = getRandom(3, 10);
+              return i[2022];
+            }),
             itemStyle: {
               normal: {
                 color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [
@@ -86,6 +124,10 @@ export const Chart2 = () => {
         ],
       })
     );
+  };
+  useEffect(() => {
+    myChart.current = echarts.init(divRef.current);
+    x(data);
   }, []);
   return (
     <div className="bordered 破获排名">
